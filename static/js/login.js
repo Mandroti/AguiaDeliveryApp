eecc61771aab92e6221a3344a15edb411c0967f7
Mandroti <karolmandroti@outlook.com>
2023-10-03 15:59:38 -0300

    03-10

---
+++ static/js/login.js
@@ -0,0 +1,198 @@
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+
+
+function exibeSenha() //precisa clicar duas vezes para alterar
+{
+    var eyeIcon = document.querySelector('.toggle-password');
+    var passwordInput = document.getElementById('password');
+    eyeIcon.addEventListener('click', function() {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            eyeIcon.querySelector('i').classList.remove('bi-eye-slash');
+            eyeIcon.querySelector('i').classList.add('bi-eye');
+        } else {
+            passwordInput.type = 'password';
+            eyeIcon.querySelector('i').classList.remove('bi-eye');
+            eyeIcon.querySelector('i').classList.add('bi-eye-slash');
+        }
+    });
+}
+
+function confirmar() //precisa clicar duas vezes para alterar
+{
+    var eyeIcon = document.querySelector('.toggle-password');
+    var passwordInput = document.getElementById('inputConfirmar');
+
+    
+    eyeIcon.addEventListener('click', function() {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            alert(passwordInput.value)
+            eyeIcon.querySelector('i').classList.remove('bi-eye-slash');
+            eyeIcon.querySelector('i').classList.add('bi-eye');
+        } else {
+            passwordInput.type = 'password';
+            eyeIcon.querySelector('i').classList.remove('bi-eye');
+            eyeIcon.querySelector('i').classList.add('bi-eye-slash');
+        }
+    });
+}
+
+function meuLogin()
+{
+    const contato = document.getElementById('username').value;
+    const senha = document.getElementById('password').value;
+    
+    const url = `https://localhost:7221/api/Usuario/Login?contato=${contato}&senha=${senha}`;
+    const dados = {
+        contato: contato,
+        senha: senha
+    };
+
+    fetch(url, { 
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },        
+    })
+    .then(response => {
+        if (!response.ok) {
+          window.location.href = "registraCliente.html"
+        }
+        else
+            window.location.href = "dashboard.html"
+        return response.json();})
+    .then(data =>{
+     })
+    .catch(error => console.error('Erro:', error));
+    event.preventDefault();
+}    
+
+
+function registraCliente()
+{
+    alert("AAAAAAAAAA")
+    var nome = document.getElementById('inputNome').value;
+        var senha = document.getElementById('password').value;
+        var confirmaSenha = document.getElementById('inputConfirmar').value;
+        var contato = document.getElementById('inputContato').value;
+        var cep = document.getElementById('inputCep').value;
+        var logradouro = document.getElementById('inputLog').value;
+        var bairro = document.getElementById('inputBairro').value;
+        var numero = document.getElementById('inputNum').value;
+        numero = parseInt(numero);
+        var cidade = document.getElementById('inputCit').value;  
+
+        const dados = {
+            Nome: nome, Senha: senha, ConfirmaSenha: confirmaSenha,
+            Contato: contato, CEP: cep, Logradouro: logradouro,
+            Bairro: bairro, Numero: numero, Cidade: cidade
+        };       
+
+        if(senha == confirmaSenha)
+        {
+            fetch('https://localhost:7221/api/Usuario/Gravar', {             
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',               
+                },
+                body: JSON.stringify(dados)           
+            })
+            .then(response => response.json())
+            .then(data =>{
+                if (!response.ok) {
+                    window.location.href = "registraCliente.html"
+                  }
+                  else
+                      window.location.href = "login.html"
+                  return response.json();})
+            .catch(error => console.error('Erro:', error));        
+            event.preventDefault();
+        }
+        else{            
+            document.getElementById('divS').style.backgroundColor = 'red';
+            document.getElementById('divC').style.backgroundColor = 'red';
+        }        
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    pedidosPendentes();
+});
+
+
+var dados = [
+    {
+        nome: "Karolaine Mandroti",
+        horario: "03 out 10:42 (há poucos segundos)",
+        endereco: "Rua Minas Gerais, 64",
+        iconeEndereco: "fas fa-utensils",
+        tipoEntrega: "Delivery",
+        iconeEntrega: "fas fa-hourglass-half"
+    },
+    // Adicione mais objetos ao array conforme necessário
+];
+
+var dados = [
+    {
+        nome: "Karolaine Mandroti",
+        horario: "03 out 10:42 (há poucos segundos)",
+        endereco: "Rua Minas Gerais, 64",
+        iconeEndereco: "fas fa-utensils",
+        tipoEntrega: "Delivery",
+        iconeEntrega: "fas fa-hourglass-half"
+    },
+    {
+        nome: "Isabela Mandroti",
+        horario: "03 out 10:42 (há poucos segundos)",
+        endereco: "Rua Minas Gerais, 64",
+        iconeEndereco: "fas fa-utensils",
+        tipoEntrega: "Delivery",
+        iconeEntrega: "fas fa-hourglass-half"
+    },
+    // Adicione mais objetos ao array conforme necessário
+];
+
+function criarCard(dado) {
+    var cardHTML = `
+        <div class="card" style="margin-right: -15px; margin-left: -15px">
+        <div style="padding: 5px">
+            <div style="display: flex;"> 
+                <h4 style="margin: 10px 0px 0px 0px;">${dado.nome}</h4>
+                <i class="${dado.iconeEndereco}" style="margin-top: 16px;"></i>
+            </div>            
+            <p style="color: lightgray;">${dado.horario}</p>
+            <div style="display: flex;">
+                <i class="${dado.iconeEndereco}" style="margin-top: 5px;"></i>
+                <p style="color: lightgray; margin-left: 10px;">${dado.endereco}</p>
+            </div>
+            <div style="display: flex; align-items: center;">
+                <i class="${dado.iconeEntrega}" style="margin-bottom: 14px;"></i>
+                <p style="color: lightgray; margin-left: 10px;">${dado.tipoEntrega}</p>
+            </div>
+        </div>           
+        </div>
+    `;
+
+    return cardHTML;
+}
+
+var pendentesContainer = document.getElementById('pendentes');
+
+for (var i = 0; i < dados.length; i++) {
+    var card = criarCard(dados[i]);
+    pendentesContainer.innerHTML += card;
+}
